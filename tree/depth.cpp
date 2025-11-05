623175d2eebc486f20a32188095901160c40b2b0
Arifin Rafi <arifinrafi247@gmail.com>
2025-11-05 17:37:45 +0600

    Deleted dublicates

--- tree/depth.cpp
+++
@@ -1,8 +0,0 @@
-void dfsDepth(ll node, ll par, mll &adj, vll &depth) {
-    for (ll child : adj[node]) {
-        if (par == child) continue;
-        depth[child] = depth[node] + 1;
-        dfsDepth(child, node, adj, depth);
-    }
-    return;
-}
