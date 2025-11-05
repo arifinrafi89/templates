c029c5e042db0b17b25c6a171448ad1f412942ed
Arifin <arifinrafi247@gmail.com>
2025-11-05 17:18:26 +0600

    folder names changed

---
+++ Templates/templateMain.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ll long long
+#define pll pair<ll, ll>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define mll vector<vll>
+#define mpll vector<vpll>
+#define pb push_back
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define all(x) x.begin(), x.end()
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+ 
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+const ll inf = LLONG_MAX;
+const ll mod = 1e9 + 7;
+const ll N = 2500;
+
+
+void solve() {
+
+    
+}
+  
+int main() {
+    fast;
+    ll tc = 1;
+    //cin >> tc;
+    for (ll i = 1; i <= tc; i++) {
+        //cout << "Case " << i << ": ";
+        //cout << '\n';
+        solve();  
+    }
+    return 0;
+ } 
--- maths/Combination/nCrModPrecalculate.cpp
+++ maths/Combination/nCrModPrecalculate.cpp
@@ -1,11 +1,11 @@
 class Combination {
 private:
     ll n, M;
-    vector<ll> fact, inv, inv_fact;
+    vector<ll> fact, inv, invFact;
 
     void init() {
         fact[0] = 1;
-        inv_fact[0] = 1;
+        invFact[0] = 1;
         for (ll i = 1; i <= n; i++) {
             if (i == 1) {
                 inv[i] = 1;
@@ -15,17 +15,17 @@ private:
                 inv[i] = (a * b) % M;
             }
             fact[i] = (fact[i - 1] * i) % M;
-            inv_fact[i] = (inv_fact[i - 1] * inv[i]) % M;
+            invFact[i] = (invFact[i - 1] * inv[i]) % M;
         }
     }
 
 public:
-    Combination(ll _n, ll _M) : n(_n), M(_M), fact(n + 1), inv(n + 1), inv_fact(n + 1) {
+    Combination(ll _n, ll _M) : n(_n), M(_M), fact(n + 1), inv(n + 1), invFact(n + 1) {
         init();
     }
 
     ll nCr(ll n, ll r) {
         if (r < 0 || r > n) return 0;
-        return (((fact[n] * inv_fact[r]) % M) * inv_fact[n - r]) % M;
+        return (((fact[n] * invFact[r]) % M) * invFact[n - r]) % M;
     }
 };
